202d97396e15e0596534ae937eb0f547ed92fbd5
ThomasPrieurD <tprieurd@gmail.com>
2016-03-01 15:36:10 +0100

    1ère tentative de push

--- db/content.sql
+++ db/content.sql
@@ -1,5 +1,8 @@
+truncate table t_pneu;
 
 insert into t_pneu values
 (1, 'Michelin', 'standard', 'neige');
 insert into t_pneu values
 (2, 'Firestone', 'petit', 'desert');
+insert into t_pneu values
+(3, 'GoodYear', 'large', 'plage');
