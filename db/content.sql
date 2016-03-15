b6ddcea22b65d84ffc1446377e120e7c843dfa75
PRIEUR-DREVON THOMAS p1511160 <tprieurd@gmail.com>
2016-03-15 17:45:39 +0100

    affichage images résolu

--- db/content.sql
+++ db/content.sql
@@ -1,10 +1,10 @@
 truncate table t_pneu;
 insert into t_pneu values
-(1, 'Michelin', 'standard', 'neige', 15.32, 'http://i2.cdscdn.com/pdt2/2/7/5/1/700x700/mic3528708577275/rw/mic-225/50r17-98w-xl-php.jpg');
+(1, 'Michelin', 'standard', 'neige', 15.32, 'pneu-michelin.jpg');
 insert into t_pneu values
-(2, 'Firestone', 'petit', 'desert', 10.21, 'http://4h10.com/wp-content/uploads/2012/01/firestone-motorcycle-500-16-10in-rgb_4.jpg');
+(2, 'Firestone', 'petit', 'desert', 10.21, 'firestone1.jpg');
 insert into t_pneu values
-(3, 'GoodYear', 'large', 'plage', 13.99, 'http://www.goodyear.eu/fr_fr/images/ts-zoom-l-GP-4D-34.jpg');
+(3, 'GoodYear', 'large', 'plage', 13.99, 'goodyear_LARGE.jpg');
 
 
 truncate table t_marque;
